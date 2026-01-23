f9ce192114575effcc11daaade4f329b8bda2f66
first@kamua <first@kamua>
2026-01-23 23:44:47 +0000

    change autoscript

--- dev/auto_process_firstpl.py
+++ dev/auto_process_firstpl.py
@@ -7,18 +7,29 @@ def find_and_process_firstpl_directories(base_dir):
             if dir_name == "firstpl":
                 firstpl_dir = os.path.join(root, dir_name)
                 print(f"Processing directory: {firstpl_dir}")
-                run_scripts_in_directory(firstpl_dir)
+                run_scripts_in_directory_only_with_modulation(firstpl_dir)
+    for root, dirs, files in os.walk(base_dir):
+        for dir_name in dirs:
             if dir_name == "darks":
                 firstpl_dir = os.path.join(root, dir_name)
                 print(f"Processing directory: {firstpl_dir}")
                 run_scripts_in_directory(firstpl_dir)
+
+def run_scripts_in_directory_only_with_modulation(directory):
+    try:
+        # Execute runPL_create_pixelMap.py
+        subprocess.run(["runPL_create_pixelMap.py"], cwd=directory, check=True)
+        # Execute runPL_make_preproc.py
+        subprocess.run(["runPL_make_preproc.py","--only_with_modulation","--overwrite"], cwd=directory, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while processing {directory}: {e}")
 
 def run_scripts_in_directory(directory):
     try:
         # Execute runPL_create_pixelMap.py
         # subprocess.run(["runPL_create_pixelMap.py"], cwd=directory, check=True)
         # Execute runPL_make_preproc.py
-        subprocess.run(["runPL_make_preproc.py","--only_with_modulation"], cwd=directory, check=True)
+        subprocess.run(["runPL_make_preproc.py"], cwd=directory, check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while processing {directory}: {e}")
 
